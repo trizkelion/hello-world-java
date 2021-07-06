d485e1d2ace2c2484cc3be74e3aa5d4a1e25fdda
Ortiz <alfonso.ortiz@audatex.com>
2021-07-06 12:25:24 -0500

    Version 2

---
+++ Jenkins.java
@@ -0,0 +1,9 @@
+public class Jenkins {
+
+	public static void main(String[] args) {		
+		System.out.println("Hola Jorge");
+		System.out.println("Hola Adolfo");
+		System.out.println("Iniciando coneccion a : Solera.com");
+	}
+
+}
